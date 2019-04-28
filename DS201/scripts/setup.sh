0234314cd306d434816ea2393dea361f1463fba9
anhtv08 <anhtv010888@gmail.com>
2019-04-28 22:27:52 +0800

    add setup scripts

---
+++ DS201/scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# if you want to use this script please update following configuration accordingly.
+export CASSANDRA_HOME='/Users/anhtrang/working/cassandra-db/apache-cassandra-3.11.4' 
+export LAB_WORK_DIR='/Users/anhtrang/working/cassandra-db/work'
+export CASSANDRA_RESOURCE_CONFIG='' # todo: to be completed
+declare -a  cassandra_course=(
+    'DS201'
+    'DS220'
+    'DS210'
+)
